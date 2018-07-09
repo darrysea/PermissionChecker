40eadf723ca100ad4de391a1b99b70445103b6ce
최병문 <darrysea@gmail.com>
2018-07-09 09:23:44 +0900

    - First Commit

---
+++ app/src/main/java/kr/co/darrysea/permissionchecker/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.darrysea.permissionchecker
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
