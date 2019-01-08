59a84e8516589c3a172a6e0251b80eb1906aafa7
최병문 <darrysea@metaps-plus.com>
2019-01-08 11:24:29 +0900

    - 풀리퀘스트 푸시 테스트

--- checker/src/main/java/kr/co/darrysea/checker/etc/TestPullRequestDummy.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.darrysea.checker.etc
-
-import android.app.Activity
-import android.content.Context
-
-class TestPullRequestDummy : Activity() {
-    private val mContext: Context = this;
-}
